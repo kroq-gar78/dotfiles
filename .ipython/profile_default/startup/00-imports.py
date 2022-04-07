e98b8b3f0a4180448b30dc9811ab392f04c0acae
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2022-04-07 00:04:02 -0500

    IPython: automatically import numpy and other utils

---
+++ .ipython/profile_default/startup/00-imports.py
@@ -0,0 +1,8 @@
+try:
+    import numpy as np
+    #import scipy as sp # might noticeably increase REPL load time
+    import pandas as pd
+    import cottoncandy as cc
+except:
+    # Failing silently is probably ok?
+    pass
