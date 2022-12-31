0215ea1e44a39baa55b3885c068481c8bf73a342
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2022-12-30 18:35:38 -0600

    waybar: add config and power menu script

---
+++ .config/sway/scripts/wofi-power.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Power menu helper script
+# Originally from:
+# https://github.com/luispabon/sway-dotfiles/blob/master/scripts/wofi-power.sh
+
+entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  logout)
+    swaymsg exit;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
